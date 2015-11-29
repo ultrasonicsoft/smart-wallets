34760f6826fac6bb6d009c1dadc15129c661c409
UVBrain services <uvbrainsolutions@outlook.com>
2015-11-29 07:30:47 +0530

    changed name of controller

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ var mainApp = angular.module('bcusSmartPayments',
         'ngMaterial',
         'ui.bootstrap'
     ])
-    .controller('SmartPaymentsCtrl',
+    .controller('AppCtrl',
         function () {
         });
 
@@ -70,7 +70,28 @@ mainApp.directive('smartPayments', function () {
     return {
         restrict: 'EA',
         scope: { data: '=' },
-        templateUrl: 'directives/smartPayments/smartPayments.html',
+        template: '<div class="container"> ' +
+            '<div ng-controller="SmartPaymentsCtrl as ctrl" ng-cloak> ' +
+            '   <div layout="row">' +
+            '      <div class="col-md-2">' +
+            '         <span class="text">Payment amount: {{ctrl.paymentDetails.amount}}</span>' +
+            '    </div>' +
+            ' </div>' +
+            '<div class="row">' +
+            '   <div class="col-md-2">' +
+            '      <span class="text">Product Info: {{ctrl.paymentDetails.productInfo}}</span>' +
+            ' </div>' +
+            '        </div>' +
+            '       <md-list ng-cloak>' +
+            '          <md-subheader class="md-no-sticky">Choose Payment Service to pay</md-subheader>' +
+            '         <md-list-item ng-repeat="paymentService in ctrl.allPaymentServices">' +
+            '            <md-button class="md-raised md-primary" ng-click="ctrl.makePayment($index)">{{ paymentService.name }}</md-button>' +
+            '       </md-list-item>' +
+            '      <md-divider></md-divider>' +
+            ' </md-list>' +
+            '<div id="payuForm"></div>' +
+            '    </div>' +
+            '</div>',
         controller: 'SmartPaymentsCtrl'
     };
 });
